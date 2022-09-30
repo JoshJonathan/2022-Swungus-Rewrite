d50564398ba7fd668d3cb98c9bad4f99f6c8ecb0
Josh Jonathan <Josh.d.jonathan@gmail.com>
2022-09-30 18:04:17 -0400

    Update IndexerSub.java

--- 2022-Swungus-Rewrite-Folder/src/main/java/frc/robot/subsystems/IndexerSub.java
+++ 2022-Swungus-Rewrite-Folder/src/main/java/frc/robot/subsystems/IndexerSub.java
@@ -51,8 +51,10 @@ public class IndexerSub extends SubsystemBase {
     if(ShooterSub.mainWheelSetpoint != Constants.SHOOTER_MAIN_WHEEL_IDLE_VELOCITY &&
        readyToShoot(ShooterSub.mainWheelSetpoint, ShooterSub.mainWheelValue, Constants.SHOOTER_MAIN_WHEEL_ALLOWABLE_ERROR) &&
        readyToShoot(ShooterSub.hoodWheelsSetpoint, ShooterSub.hoodWheelsValue, Constants.SHOOTER_HOOD_WHEELS_ALLOWABLE_ERROR) &&
-       readyToShoot(ShooterSub.kickerWheelSetpoint, ShooterSub.kickerWheelValue, Constants.SHOOTER_KICKER_WHEEL_ALLOWABLE_ERROR)
-    ) {
+       readyToShoot(ShooterSub.kickerWheelSetpoint, ShooterSub.kickerWheelValue, Constants.SHOOTER_KICKER_WHEEL_ALLOWABLE_ERROR) &&
+       readyToShoot(ShooterSub.servoSetpoint, ShooterSub.servoValue, Constants.SHOOTER_SERVOS_ALLOWABLE_ERROR) &&
+       (readyToShoot(0, LimelightSub.x, Constants.DRIVETRAIN_ALLOWABLE_ERROR) || ShooterSub.mainWheelSetpoint == Constants.SHOOTER_MAIN_WHEEL_FENDERSHOT_VELOCITY)	
+       ) {
       index(Constants.INDEXER_OUTPUT);
       timer.reset();
       timer.start();
